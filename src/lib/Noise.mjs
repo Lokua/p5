a76b310509d0009b9862b7a4d60f54f2601254ab
Lokua <dev@lokua.net>
2024-11-01 20:12:29 -0500

    feat: add simplex-noise

---
+++ src/lib/Noise.mjs
@@ -0,0 +1,38 @@
+import { createNoise2D, createNoise3D, createNoise4D } from 'simplex-noise'
+import alea from './alea.mjs'
+
+export class Simplex {
+  constructor(type = '2d', seed) {
+    this.#createNoiseImplemenation(type, seed)
+  }
+
+  #createPrng(seed) {
+    return alea(seed)
+  }
+
+  #createNoiseImplemenation(type, seed) {
+    this.type = type.toLowerCase()
+    this.seed = seed
+    this.prng = this.#createPrng(seed ?? Math.random())
+    this.prng = this.prng.bind(this)
+    this.noiseImplemenation =
+      type === '2d'
+        ? createNoise2D(this.prng)
+        : type === '3d'
+          ? createNoise3D(this.prng)
+          : createNoise4D(this.prng)
+  }
+
+  noise(...args) {
+    // [0, 1] instead of [-1, 1]
+    return (this.noiseImplemenation(...args) + 1) / 2
+  }
+
+  setSeed(seed) {
+    this.seed = seed
+    this.#createNoiseImplemenation(this.type, this.seed)
+  }
+  noiseSeed(seed) {
+    this.setSeed(seed)
+  }
+}
---
+++ src/lib/alea.mjs
@@ -0,0 +1,89 @@
+// modernized from https://www.npmjs.com/package/alea
+//---
+// From http://baagoe.com/en/RandomMusings/javascript/
+
+// importState to sync generator states
+Alea.importState = (state) => {
+  const random = new Alea()
+  random.importState(state)
+  return random
+}
+
+export default Alea
+
+function Alea(...args) {
+  // Johannes Baagøe <baagoe@baagoe.com>, 2010
+
+  // Initial state variables
+  let s0 = 0
+  let s1 = 0
+  let s2 = 0
+  let c = 1
+
+  // Use current time if no arguments are provided
+  args = args.length === 0 ? [+new Date()] : args
+
+  // Initialize mash function and state
+  let mash = Mash()
+  s0 = mash(' ')
+  s1 = mash(' ')
+  s2 = mash(' ')
+
+  for (const arg of args) {
+    s0 -= mash(arg)
+    if (s0 < 0) s0 += 1
+    s1 -= mash(arg)
+    if (s1 < 0) s1 += 1
+    s2 -= mash(arg)
+    if (s2 < 0) s2 += 1
+  }
+  mash = null
+
+  // Random number generator function
+  const random = () => {
+    const t = 2091639 * s0 + c * 2.3283064365386963e-10 // 2^-32
+    s0 = s1
+    s1 = s2
+    s2 = t - (c = t | 0)
+    return s2
+  }
+
+  // Additional methods
+  random.next = random
+  random.uint32 = () => random() * 0x100000000 // 2^32
+  random.fract53 = () =>
+    random() + ((random() * 0x200000) | 0) * 1.1102230246251565e-16 // 2^-53
+  random.version = 'Alea 0.9'
+  random.args = args
+
+  // Export and import state
+  random.exportState = () => [s0, s1, s2, c]
+  random.importState = ([s0New, s1New, s2New, cNew]) => {
+    s0 = +s0New || 0
+    s1 = +s1New || 0
+    s2 = +s2New || 0
+    c = +cNew || 0
+  }
+
+  return random
+}
+
+function Mash() {
+  let n = 0xefc8249d
+  const mash = (data) => {
+    data = data.toString()
+    for (const char of data) {
+      n += char.charCodeAt(0)
+      let h = 0.02519603282416938 * n
+      n = h >>> 0
+      h -= n
+      h *= n
+      n = h >>> 0
+      h -= n
+      n += h * 0x100000000 // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10 // 2^-32
+  }
+  mash.version = 'Mash 0.9'
+  return mash
+}
